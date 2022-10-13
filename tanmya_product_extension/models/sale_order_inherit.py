5fffd939404f3fbcb11820db3487d1691e892b86
differs-dev <103491986+differs-dev@users.noreply.github.com>
2022-10-13 16:43:50 +0300

    Update sale_order_inherit.py

--- tanmya_product_extension/models/sale_order_inherit.py
+++ tanmya_product_extension/models/sale_order_inherit.py
@@ -264,9 +264,9 @@ class SaleOrderInerit(models.Model):
                         where aa.ref='{inv_name}' or aa.move_name='{inv_name}'
                         """
                 self._cr.execute(qry)
-                self.create_cash_statement(inv_name, pay_action)
-                if seq_transaction % 10 == 0:
-                    self._cr.commit()
+#                 self.create_cash_statement(inv_name, pay_action)
+#                 if seq_transaction % 10 == 0:
+#                     self._cr.commit()
                 ###############################################
 
     @api.model
