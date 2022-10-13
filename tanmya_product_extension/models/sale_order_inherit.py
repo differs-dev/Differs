bd730c5454d54dc578890cd7ff71c5c2cab6cb6c
differs-dev <103491986+differs-dev@users.noreply.github.com>
2022-10-13 17:13:40 +0300

    Update sale_order_inherit.py

--- tanmya_product_extension/models/sale_order_inherit.py
+++ tanmya_product_extension/models/sale_order_inherit.py
@@ -186,9 +186,10 @@ class SaleOrderInerit(models.Model):
         annos = self.env['account.reconcile.model'].sudo().browse(1)
         annos._apply_rules(statement.line_ids)
         _logger.info('------------------------++++++++++++++++++++++++++++++++++++++++++++++++++')
-        _logger.info(annos)
-        _logger.info(annos._apply_rules(statement.line_ids))
         _logger.info(statement.state)
+        _logger.info(statement.line_ids[0].payment_ref)
+        _logger.info(statement.line_ids[0].partner_id.name)
+        _logger.info(statement.line_ids[0].amount)
         _logger.info(statement.all_lines_reconciled)
         for line in statement.line_ids:
             _logger.info(line.is_reconciled)
