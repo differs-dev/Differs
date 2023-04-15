d7f2ec410510232e98214e2423997977c81bf28a
differs-dev <103491986+differs-dev@users.noreply.github.com>
2023-04-15 14:45:39 +0300

    Delete tanmya_product_extension directory

--- tanmya_product_extension/models/product_template_inherit.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class ProductTemplateInherit(models.Model):
-    _inherit = "product.template",
-
-    website_description = fields.Char('Description for the website')
