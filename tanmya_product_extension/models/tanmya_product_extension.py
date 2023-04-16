7ba345be4a6ebaf546b0104963ab012dbd7779c5
differs-dev <103491986+differs-dev@users.noreply.github.com>
2023-04-16 09:50:07 +0200

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -455,6 +455,7 @@ class TanmyaProducExt(models.Model):
         search_word1 = search_word.capitalize()
         search_word2 = search_word.lower()
         search_word3 = search_word.upper()
+        _logger.info(category_id)
         if category_id > 0:
             products = self.env['product.template'].sudo().search(['|', '|', '|',
                                                                   ('name', 'like', search_word),
