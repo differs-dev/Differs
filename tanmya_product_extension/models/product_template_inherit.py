47513e5c165fea80d11468de4c1bba361fb33901
differs-dev <103491986+differs-dev@users.noreply.github.com>
2023-04-16 13:17:14 +0300

    Update product_template_inherit.py

--- tanmya_product_extension/models/product_template_inherit.py
+++ tanmya_product_extension/models/product_template_inherit.py
@@ -5,6 +5,8 @@ class ProductTemplateInherit(models.Model):
     _inherit = "product.template",
 
     description = fields.Char('Description', translate=True)
+    website_description = fields.Char('Description for the website', sanitize_attributes=False,
+                                      translate=True, sanitize_form=False)
     
     # Nutrition Value Fields
     calories = fields.Char(string='Calories')
