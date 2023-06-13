8a289e0002564289da78ec66939db30a6abe8155
differs-dev <103491986+differs-dev@users.noreply.github.com>
2023-06-13 13:08:27 +0300

    Update firebase_notification.py

--- altanmya_firebase_notificator/models/firebase_notification.py
+++ altanmya_firebase_notificator/models/firebase_notification.py
@@ -211,35 +211,50 @@ class FirebaseNotification(models.Model):
                 return notification
 
             else:
-                if not self.notification_date:
-                    self.notification_date = datetime.now()
-                message = messaging.MulticastMessage(
-                    notification=messaging.Notification(
-                        title=self.title,
-                        body=self.content,
-                    ),
-                    android=messaging.AndroidConfig(priority='high',
-                                                    notification=messaging.AndroidNotification(sound='default',
-                                                                                               click_action=self.target_action,
-                                                                                               color='#61B559',
-                                                                                               title=title,
-                                                                                               body=content
-                                                                                               )),
-                    data={'ios_click_action': self.target_action,
-                          'recipe_id': str(self.recipe_id),
-                          'payload': self.payload},
-                    apns=messaging.APNSConfig(payload=messaging.APNSPayload(aps=messaging.Aps(sound='default',
-                                                                                              alert=messaging.ApsAlert(
-                                                                                                  title=title,
-                                                                                                  body=content
-                                                                                              )))),
-                    tokens=tokens,
-                )
-                response = messaging.send_multicast(message, app=firebase_app)
+                responses = []
+                successful_responses = []
+                for user in self.user_ids:
+                    tokens = user.mapped('firebase_account_id').mapped('token')
+                    if user.preferred_language == 'en':
+                        title = self.title
+                        content = self.content
+                    else:
+                        title = self.fr_title
+                        content = self.fr_content
+
+                    if not self.notification_date:
+                        self.notification_date = datetime.now()
+                    message = messaging.MulticastMessage(
+                        notification=messaging.Notification(
+                            title=title,
+                            body=content,
+                        ),
+                        android=messaging.AndroidConfig(priority='high',
+                                                        notification=messaging.AndroidNotification(sound='default',
+                                                                                                   click_action=self.target_action,
+                                                                                                   color='#61B559',
+                                                                                                   title=title,
+                                                                                                   body=content
+                                                                                                   )),
+                        data={'ios_click_action': self.target_action,
+                              'recipe_id': str(self.recipe_id),
+                              'payload': self.payload},
+                        apns=messaging.APNSConfig(payload=messaging.APNSPayload(aps=messaging.Aps(sound='default',
+                                                                                                  alert=messaging.ApsAlert(
+                                                                                                      title=title,
+                                                                                                      body=content
+                                                                                                  )))),
+                        tokens=tokens,
+                    )
+                    response = messaging.send_multicast(message, app=firebase_app)
+                    responses.append(response)
+                    if response.success_count > 0:
+                        successful_responses.append(responses)
+
                 odoo_notification_title = ''
                 odoo_notification_message = ''
                 odoo_notification_type = ''
-                if response.success_count > 0:
+                if len(responses) == len(successful_responses):
                     odoo_notification_title = 'Successfully!'
                     odoo_notification_message = f'The notification have been sent to {response.success_count} user.'
                     odoo_notification_type = 'success'
