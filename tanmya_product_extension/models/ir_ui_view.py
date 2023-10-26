547805ded5a82c12104be1da0b5839a9d7f6b753
mouhuopuo <142087087+mouhuopuo@users.noreply.github.com>
2023-10-26 16:10:44 +0300

    Add files via upload

---
+++ tanmya_product_extension/models/ir_ui_view.py
@@ -0,0 +1,26 @@
+import logging
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
+
+
+class View(models.Model):
+    _inherit = 'ir.ui.view'
+
+    @api.constrains('type', 'groups_id', 'inherit_id')
+    def _check_groups(self):
+        for view in self:
+            _logger.info('///////////////////////////////////////')
+            _logger.info(view.name)
+            _logger.info('////////////////////////////////////////')
+            if (view.type == 'qweb' and
+                    view.groups_id and
+                    view.inherit_id and
+                    view.mode != 'primary'):
+                _logger.info('11111111111111111111111111111111111')
+                _logger.info(view.name)
+                _logger.info('11111111111111111111111111111111111')
+                raise ValidationError(
+                    _("Inherited Qweb view cannot have 'Groups' define on the record. Use 'groups' attributes inside the view definition"))
+
