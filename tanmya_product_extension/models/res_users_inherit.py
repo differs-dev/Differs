3017b4cb559106106c7e4a18297b693f17c7b405
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-04 12:25:35 +0300

    Update res_users_inherit.py

--- tanmya_product_extension/models/res_users_inherit.py
+++ tanmya_product_extension/models/res_users_inherit.py
@@ -246,6 +246,9 @@ class ResUsers(models.Model):
                     _logger.info('product_id : ')
                     _logger.info(product_preference.product_id.id)
                     _logger.info(product_preference.template_id.id)
+                    _logger.info('variants')
+                    _logger.info(self.get_products_variants_details(product_preference.id))
+                    _logger.info(self.get_products_variants_details(product_preference.product_id.id)
                     user_preference = {
                         'id': product_preference.template_id.id,
                         'image_128': product_preference.template_id.image_1920,
