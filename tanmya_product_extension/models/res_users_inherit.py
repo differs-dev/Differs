1a09b3fdae2fd79078c0deb78513ebc2a3f85787
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-04 12:34:22 +0300

    Update res_users_inherit.py

--- tanmya_product_extension/models/res_users_inherit.py
+++ tanmya_product_extension/models/res_users_inherit.py
@@ -79,7 +79,7 @@ class ResUsers(models.Model):
                 _logger.info(product_id)
                 prod = self.env['product.product'].search([('id', '=', product_id)])
                 product_preferences_vals = {
-                    'product_id': product_id,
+                    'product_id': prod.id,
                     # 'template_id': product_id,
                     'status': product_status
                 }
