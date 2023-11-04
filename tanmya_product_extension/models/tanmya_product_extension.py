0b030e20e0659ecfc28c7bb09b3ad144d8ab766d
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-04 20:15:10 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -407,8 +407,7 @@ class TanmyaProducExt(models.Model):
             for i in range(len(vals.get('ingredients_names'))):
                 _logger.info('uom_ids are : ')
                 _logger.info(vals.get('uom_id'))
-                _logger.info(type(vals.get('uom_id')))
-                if type(vals.get('uom_id')) == 'int':
+                if isinstance(vals.get('uom_id'), int):
                     uom_id = 1
                 else:
                     uom_id = self.env['uom.uom'].sudo().search([('name', '=', vals.get('uom_id')[i])], limit=1).id
