bc3514b153f5c91e833149c51932059655ea1a6c
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-04 18:10:10 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -522,9 +522,9 @@ class TanmyaProducExt(models.Model):
                         for line in sale_order_template.sale_order_template_line_ids:
                             line.unlink()
                         for i in range(len(vals.get('ingredients_products'))):
-                             uom_id = self.env['uom.uom'].sudo().search([('name', '=', vals.get('uom_id')[i])], limit=1).id
-                             _logger.info('uom are : ')
-                             _logger.info(uom_id)
+                            uom_id = self.env['uom.uom'].sudo().search([('name', '=', vals.get('uom_id')[i])], limit=1).id
+                            _logger.info('uom are : ')
+                            _logger.info(uom_id)
                             sale_order_template_line_vals = {
                                 'name': vals.get('ingredients_names')[i],
                                 'sale_order_template_id': sale_order_template.id,
