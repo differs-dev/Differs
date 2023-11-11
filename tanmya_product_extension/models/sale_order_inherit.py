10af3fc578e426d9fe045c778c17509e2c77888a
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-11 18:06:54 +0300

    Update sale_order_inherit.py

--- tanmya_product_extension/models/sale_order_inherit.py
+++ tanmya_product_extension/models/sale_order_inherit.py
@@ -359,18 +359,18 @@ class SaleOrderInerit(models.Model):
             rec_date_order2 = rec_date_order + timedelta(hours=3)  # diff_hour
             rec_date_order_invoice = datetime(rec_date_order2.year, rec_date_order2.month, rec_date_order2.day)
 
-            if rec.state == 'sale':
-                try:
-                    super(SaleOrderInerit, rec)._action_cancel()
-                except:
-                    super(SaleOrderInerit, rec).action_cancel()
-                rec.action_draft()
-                # rec._modify_corder()
-                try:
-                    rec.saletype = False
-                except:
-                    pass
-                rec.action_confirm()
+            # if rec.state == 'sale':
+            #     try:
+            #         super(SaleOrderInerit, rec)._action_cancel()
+            #     except:
+            #         super(SaleOrderInerit, rec).action_cancel()
+            #     rec.action_draft()
+                # # rec._modify_corder()
+                # try:
+                #     rec.saletype = False
+                # except:
+                #     pass
+                # rec.action_confirm()
 
                 rec.date_order = rec_date_order
                 super(SaleOrderInerit, rec)._create_invoices(final=True)
