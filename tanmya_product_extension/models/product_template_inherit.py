4305b55720e7b5d6720cc82703deab17d1dc5621
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-11 19:51:05 +0300

    Update product_template_inherit.py

--- tanmya_product_extension/models/product_template_inherit.py
+++ tanmya_product_extension/models/product_template_inherit.py
@@ -43,7 +43,9 @@ class ProductTemplateInherit(models.Model):
 
     def compute_price_from_pricelist(self):
         price_list = self.env['product.pricelist'].with_context(lang='en_US').sudo().search([('name', 'like', 'X1.5')])
-        product = self.env['product.product'].sudo().search([('name', '=', self.name)], limit=1)
+        product = self.env['product.product'].sudo().with_lang(lang='en_US').search([('name', '=', self.name)], limit=1)
+        _logger.info('---------------- targeted prod -----------------------------')
+        _logger.info(product)
         if product :
             price = price_list.get_product_price(product.product_variant_ids[0], 1, False)
             return price
