f8bd536f29a49276cdc820936d8c6325261cd7b3
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-11-18 10:20:53 +0300

    Update sale_order_inherit.py

--- tanmya_product_extension/models/sale_order_inherit.py
+++ tanmya_product_extension/models/sale_order_inherit.py
@@ -204,6 +204,11 @@ class SaleOrderInerit(models.Model):
                 if i > offset:
                     break
                 if line.product_id.detailed_type != 'service':
+                    product = self.env['product.product'].sudo().search([('product_tmpl_id', '=', line.product_id.id)])
+                    if product.recipe_status == 'public':
+                        price = product.lst_price
+                    else:
+                        price = product.product_tmpl_id.compute_variant_price_from_pricelist(product.id)
                     line_details = {
                         'id': line.id,
                         'product': {
@@ -212,7 +217,8 @@ class SaleOrderInerit(models.Model):
                             # this line was changed from this :  line.product_id.image_1920 to this ''.
                             'image': line.product_id.image_1920 or '',
                             'product_attributes': self.get_variant_attributes(line.product_id.id)},
-                        'price': line.price_unit,
+                        # 'price': line.price_unit,
+                        'price': price,
                         'quantity': line.product_uom_qty,
                         'uom': line.product_uom.name,
                         'currency': line.currency_id.name,
