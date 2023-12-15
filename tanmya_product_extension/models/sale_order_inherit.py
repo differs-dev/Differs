c2e3a5983f7145ad03ffed5c0ff1d6b1d37cff28
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-12-15 13:53:49 +0300

    Update sale_order_inherit.py

--- tanmya_product_extension/models/sale_order_inherit.py
+++ tanmya_product_extension/models/sale_order_inherit.py
@@ -67,11 +67,11 @@ class SaleOrderInerit(models.Model):
             user_sale_order = self.env['sale.order'].with_context(lang=user_lang).sudo().search([('partner_id', '=', user.partner_id.id),
                                                                     ('state', '=', 'draft')],
                                                                    order='date_order desc', limit=1)
-            _logger.info(f'user sale order : {user_sale_order)')
+            _logger.info(f'user sale order : {user_sale_order}')
             user_sale_order_not_localized = self.env['sale.order'].sudo().search([('partner_id', '=', user.partner_id.id),
                                                                     ('state', '=', 'draft')],
                                                                    order='date_order desc', limit=1)
-            _logger.info(f'user sale order not localized : {user_sale_order_not_localized)')
+            _logger.info(f'user sale order not localized : {user_sale_order_not_localized}')
             total_service_lines = 0
             order_lines = self.env['sale.order.line'].sudo().search([('order_id', '=', user_sale_order.id)])
             for line in order_lines:
