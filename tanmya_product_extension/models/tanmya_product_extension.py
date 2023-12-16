c1fe7d07e9153cce00827d961a6f467e956163f1
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-12-16 14:42:51 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -223,7 +223,8 @@ class Tanmyaprodcategory(models.Model):
                 category_details = {
                     'id': category.id,
                     'name': category.name,
-                    'image': category.image
+                    'image': category.image,
+                    'color': category.mobile_color
                 }
                 categories_details.append(category_details)
             return categories_details
