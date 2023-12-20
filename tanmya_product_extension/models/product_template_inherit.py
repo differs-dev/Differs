7acb460e4e2e5b390993d9f2a081a577fca2d5cf
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-12-20 19:47:57 +0300

    Update product_template_inherit.py

--- tanmya_product_extension/models/product_template_inherit.py
+++ tanmya_product_extension/models/product_template_inherit.py
@@ -52,14 +52,16 @@ class ProductTemplateInherit(models.Model):
 
     def compute_price_field_from_pricelist(self):
         price_list = self.env['product.pricelist'].with_context(lang='en_US').sudo().search([('is_mobile_list', '=', True)])
-        if price_list:
-            for rec in self:
+        for rec in self:
+            if price_list:
                 product = self.env['product.product'].sudo().search([('product_tmpl_id', '=', rec.id)])
                 if product :
                     price = price_list.get_product_price(product.product_variant_ids[0], 1, False)
                     rec.price_based_on_price_list = price
                 else:
                     rec.price_based_on_price_list = 0.0
+            else:
+                rec.price_based_on_price_list = 0.0
 
     def compute_price_from_pricelist(self, product_id):
         price_list = self.env['product.pricelist'].with_context(lang='en_US').sudo().search([('is_mobile_list', '=', True)])
@@ -73,17 +75,22 @@ class ProductTemplateInherit(models.Model):
                 return price
             else:
                 return 0.0
+        else:
+            return 0.0
             
     def compute_variant_price_from_pricelist(self, product_id):
         price_list = self.env['product.pricelist'].with_context(lang='en_US').sudo().search([('is_mobile_list', '=', True)])
         product = self.env['product.product'].sudo().search([('id', '=', product_id)])
         _logger.info(product)
         if price_list:
-                if product :
-                    price = price_list.get_product_price(product, 1, False)
-                    return price
-                else:
-                    return 0.0
+            if product :
+                price = price_list.get_product_price(product, 1, False)
+                return price
+            else:
+                return 0.0
+        else:
+            return 0.0
+                    
 
     def convert_list_to_string(self, nut_list):
         result = ''
