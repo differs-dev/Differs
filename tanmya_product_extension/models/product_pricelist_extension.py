a29d2f66d33dc5bfce9fbc8f921292da7bc60d45
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-12-20 19:35:02 +0300

    Update product_pricelist_extension.py

--- tanmya_product_extension/models/product_pricelist_extension.py
+++ tanmya_product_extension/models/product_pricelist_extension.py
@@ -1,7 +1,14 @@
 from odoo import api, fields, models, tools
+from odoo.exceptions import ValidationError
 
 
 class ProductPricelist(models.Model):
     _inherit = "product.pricelist"
     is_mobile_list = fields.Boolean("Use this list in mobile app?")
-  
+
+    @api.constrains('is_mobile_list')
+    def check_if_mobile_has_list(self):
+        lists = self.env['product.pricelist'].sudo().search([])
+        for list in lists:
+            if list.is_mobile_list:
+                raise ValidationError("Another price list is a mobile pricelist, please deactivate 'Use this list in mobile app?' if you want to use this list ")
