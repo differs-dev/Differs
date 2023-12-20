905c6c917c5d81b5ea83e62ba8e36786975d5309
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2023-12-20 19:36:09 +0300

    Update product_pricelist_extension.py

--- tanmya_product_extension/models/product_pricelist_extension.py
+++ tanmya_product_extension/models/product_pricelist_extension.py
@@ -10,5 +10,6 @@ class ProductPricelist(models.Model):
     def check_if_mobile_has_list(self):
         lists = self.env['product.pricelist'].sudo().search([])
         for list in lists:
-            if list.is_mobile_list:
-                raise ValidationError("Another price list is a mobile pricelist, please deactivate 'Use this list in mobile app?' if you want to use this list ")
+            if list.is_mobile_list and list.id != self.id:
+                raise ValidationError("Another price list is a mobile pricelist,
+                please deactivate 'Use this list in mobile app?' if you want to use this list ")
