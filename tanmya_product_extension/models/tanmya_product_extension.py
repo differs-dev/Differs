1fb153a5501d94c4fe52a635ac655f72830500e9
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2024-01-05 16:10:23 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -759,7 +759,7 @@ class TanmyaProducExt(models.Model):
 
             products_details = []
             for product in products:
-                price1 = self.compute_variant_price_from_pricelist(product.id)
+                price1 = self.product_tmpl_id.compute_price_from_pricelist(self.product_tmpl_id.id)
                 product_details = {
                     'id': product.id,
                     'name': product.name,
