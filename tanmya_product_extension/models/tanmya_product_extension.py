a7ac2f4f772be8244ef1345df4037ea5ade600f0
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2024-01-05 16:30:38 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -464,7 +464,7 @@ class TanmyaProducExt(models.Model):
                     'sale_order_template_id': sale_order_template_id.id,
                     'product_id': vals.get('ingredients_products')[i],
                     'product_uom_qty': vals.get('ingredients_qty')[i],
-                    'product_uom_id': uom_id
+                    'product_uom_id': 1
                 }
                 self.env['sale.order.template.line'].sudo().create(sale_order_template_line_vals)
             if vals.get('recipe_image') == 'New':
