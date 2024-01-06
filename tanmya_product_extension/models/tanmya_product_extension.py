aabdee802c020a5b8b052410acc889137c58f8ae
AnasBakkarTA <130453075+AnasBakkarTA@users.noreply.github.com>
2024-01-06 20:15:44 +0300

    Update tanmya_product_extension.py

--- tanmya_product_extension/models/tanmya_product_extension.py
+++ tanmya_product_extension/models/tanmya_product_extension.py
@@ -796,7 +796,7 @@ class TanmyaProducExt(models.Model):
         for line in sale_order.sale_order_template_line_ids:
             _logger.info(f'ing name : {line.name}')
             _logger.info(f'ing id : {line.id}')
-            price = line.product_id.product_tmpl_id.compute_price_from_pricelist(line.product_id.id)
+            price = line.product_id.product_tmpl_id.compute_price_from_pricelist(line.product_id.product_tmpl_id.id)
             ingredient_details = {
                 'id': line.id,
                 'name': line.product_id.name,
